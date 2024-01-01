8adaf2ed82e6bb90cb96584d4575898ff41dce5c
xabhishekhar <72898173+xabhishekhar@users.noreply.github.com>
2024-01-01 13:02:49 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -37,14 +37,14 @@ async def render_page(id, secure_hash):
     current_url = f'{Var.URL}/{str(id)}/{file_data.file_name}?hash={secure_hash}'
     html_code = f'''
    <p>
-    <center><h5>Click on 👇 button to watch/download in your favorite player</h5></center>
+    <center><h5>Click On 👇 Buttons To Watch/Download In Your Favorite Player</h5></center>
     <center>
-        <button style="font-size: 20px; background-color: skyblue; border-radius: 10px;" onclick="window.location.href = 'intent:{current_url}#Intent;package=com.mxtech.videoplayer.ad;S.title={file_data.file_name};end'">MX player</button> &nbsp
-        <button style="font-size: 20px; background-color: orange; border-radius: 10px;" onclick="window.location.href = 'vlc://{current_url}'">VLC player</button> &nbsp <br>
+        <button style="font-size: 40px; background-color: skyblue; border-radius: 20px;" onclick="window.location.href = 'intent:{current_url}#Intent;package=com.mxtech.videoplayer.ad;S.title={file_data.file_name};end'">MX Player</button> &nbsp
+        <button style="font-size: 40px; background-color: orange; border-radius: 20px;" onclick="window.location.href = 'vlc://{current_url}'">VLC Player</button> &nbsp <br>
         <p>&nbsp</p>
-        <button style="font-size: 20px; background-color: red; border-radius: 10px;" onclick="window.location.href = 'playit://playerv2/video?url={current_url}&amp;title={file_data.file_name}'">Playit player</button> &nbsp <br>
+        <button style="font-size: 40px; background-color: red; border-radius: 20px;" onclick="window.location.href = 'playit://playerv2/video?url={current_url}&amp;title={file_data.file_name}'">Playit Player</button> &nbsp <br>
         <p>&nbsp</p>
-        <button style="font-size: 20px; background-color: yellow; border-radius: 10px;" onclick="window.location.href = '{current_url}'">Save in your gallery</button> &nbsp
+        <button style="font-size: 40px; background-color: yellow; border-radius: 20px;" onclick="window.location.href = '{current_url}'">Download Video In Gallery</button> &nbsp
     </center>
 </p>
 </p>
